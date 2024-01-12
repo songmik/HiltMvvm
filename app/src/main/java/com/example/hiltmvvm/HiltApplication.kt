9cc3c5a92ce08f7715f2bb87f27c82662b48d3ee
kwonsongmi <ksongmi0727@gmail.com>
2024-01-12 14:45:52 +0900

    Application initialized

--- app/src/main/java/com/example/hiltmvvm/HiltApplication.kt
+++ app/src/main/java/com/example/hiltmvvm/HiltApplication.kt
@@ -1,8 +1,12 @@
 package com.example.hiltmvvm
 
 import android.app.Application
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class HiltApplication: Application() {
+class HiltApplication: Application(), Configuration.Provider {
+    override val workManagerConfiguration: Configuration = Configuration.Builder()
+        .setMinimumLoggingLevel(android.util.Log.DEBUG)
+        .build()
 }
